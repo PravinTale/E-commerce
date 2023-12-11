d67f3d9a206f912b63d274707072b79ac7ecd5d9
Pravin Tale <pravintale201700@gmail.com>
2023-12-11 19:07:34 +0530

    User entity class created

---
+++ src/main/java/com/newproject/ecomm/model/User.java
@@ -0,0 +1,31 @@
+package com.newproject.ecomm.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer userId;
+
+    @NotNull(message = "Please enter name")
+    private String name;
+    @NotNull(message = "Please enter mobile number")
+    private String mobile;
+    @NotNull(message = "Please enter password")
+    @Pattern(regexp = "[7896]{1}[0-9]{9}", message = "Input a valid mobile number")
+    private String password;
+
+
+}
